af85ebb79f1584884d646833722f610279d51098
next51 <43142523+next51@users.noreply.github.com>
2018-11-21 14:03:05 +0200

    Add files via upload

---
+++ grafiks.py
@@ -0,0 +1,21 @@
+import sys
+sys.path.append('/usr/local/anaconda3/lib/python3.6/site-packages')
+
+from numpy import cos,linspace
+x = linspace(0,7,70)
+y = cos(x)
+
+from matplotlib import pyplot as plt
+plt.grid()
+plt.xlabel('x')
+plt.ylabel('f(x)')
+plt.title('Funkcijas $cos(x)$ un $sin(x)$')
+#plt.plot(x,y)
+plt.plot(x, y, color = "00BB00")
+
+y2= sin(x)
+plt.plot(x, y2, color = "0000RR")
+
+plt.legend(['$cos(x)$','$sin(x)$'])
+
+plt.show()
