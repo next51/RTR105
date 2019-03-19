add262eb93c3f3fb40d61df0047368c05449a2fa
next51 <43142523+next51@users.noreply.github.com>
2019-03-19 15:42:52 +0200

    Update Secondpart_Matlab.m

--- Secondpart_Matlab.m
+++ Secondpart_Matlab.m
@@ -7,4 +7,13 @@ plot(x,y,'k')
 grid on
 xlabel('x')
 ylabel('y')
-title('f(x)=3*x^2')
+title('Otra uzdevuma grafiki')
+hold on
+syms X
+X = X^3;
+f = int(X);
+X=0:0.01:5;
+Y=eval(vectorize(f));
+plot(X,Y)
+legend('y=3x^2','y=x^4/4')
+hold off
